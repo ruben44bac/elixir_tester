3ffbc486188e1aa43b633c9b54c08f4cdf991296
ruben.baeza <ruben.baeza@santiago.mx>
2019-10-24 21:36:29 -0500

    integracion de tailwind y avance en layout

---
+++ apps/guiltySpark_web/assets/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        indigo: {
+          '100': '#F7FCFF' 
+        }
+      }
+    },
+    fontFamily: {
+      'display': ['Oswald', 'sans-serif'],
+      'ubuntu': ['Ubuntu', 'sans-serif'],
+      'error': ['Dosis','sans-serif']
+    }
+  },
+  variants: {},
+  plugins: []
+}
--- apps/guiltySpark_web/lib/guiltySpark_web/views/layout_view.ex
+++ apps/guiltySpark_web/lib/guiltySpark_web/views/layout_view.ex
@@ -1,3 +1,12 @@
 defmodule GuiltySparkWeb.LayoutView do
   use GuiltySparkWeb, :view
+
+  def user_email(_conn) do
+    "rubeng44@hotmail.com"
+  end
+
+  def valid_session(conn) do
+    true
+  end
+
 end
