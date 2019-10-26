c5255267b09e39b4f86c52ba547f6d15faa00fd8
ruben.baeza <ruben.baeza@santiago.mx>
2019-10-26 01:35:20 -0500

    usuario de fluffy

--- apps/guiltySpark/lib/guiltySpark/handler/session_handler.ex
+++ apps/guiltySpark/lib/guiltySpark/handler/session_handler.ex
@@ -1,6 +1,7 @@
 defmodule GuiltySpark.PermissionHandler do
 
-
+  #@fluffy_ip "10.0.3.120"
+  @fluffy_ip "172.19.1.1"
   def validate_fluffy(params, conn) do
 
     case params == %{} do
@@ -14,7 +15,7 @@ defmodule GuiltySpark.PermissionHandler do
 
   def validate_fluffy(token) do
     req = JSX.encode! %{"token" => token}
-    case HTTPoison.post("http://172.19.1.1:4000/api/login/verify", req, [{"Content-Type", "application/json"}]) do
+    case HTTPoison.post("http://#{@fluffy_ip}:4000/api/login/verify", req, [{"Content-Type", "application/json"}]) do
       {:ok, %HTTPoison.Response{status_code: 200}} ->
         true
       {:ok, _} ->
@@ -44,10 +45,7 @@ defmodule GuiltySpark.PermissionHandler do
         "token" => token,
         "path" => Base.encode64(program)}
 
-      HTTPoison.get("http://172.19.1.1:4000/api/permission", [], params: req)
-        |> IO.inspect(label: "RESPUESTA DE SERVICIO DEL PEDRON =============>>>>>    ")
-
-      case HTTPoison.get("http://172.19.1.1:4000/api/permission", [], params: req) do
+      case HTTPoison.get("http://#{@fluffy_ip}:4000/api/permission", [], params: req) do
         {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
           body
            |> String.replace("'", "\"")
@@ -56,6 +54,44 @@ defmodule GuiltySpark.PermissionHandler do
         {:error, _} -> %{}
       end
   end
+
+
+  def get_user_names(params, conn, _attrs) do
+    case params == %{} do
+        true -> Plug.Conn.get_session(conn, :user_id)
+            |> get_user_names(Plug.Conn.get_session(conn, :role_id))
+        false -> get_user_names(params)
+    end
+  end
+
+  def get_user_names(%{"uid" => user_id, "rid" => role_id }), do: get_user_names(user_id, role_id)
+
+  def get_user_names(user_id, role_id) do
+    req = %{"user_id" => user_id}
+    case HTTPoison.get("http://#{@fluffy_ip}:4000/api/roles/id", [], params: req) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        body
+         |> String.replace("'", "\"")
+         |> Poison.decode!
+         |> select_user(role_id)
+      {:ok, _} -> %{}
+      {:error, _} -> %{}
+    end
+  end
+
+  def select_user(%{"data" => %{"roles" => roles}}, role_id) do
+    roles_array(roles, String.to_integer(role_id))
+  end
+
+  def roles_array([head | teal], role_id) do
+    if head["role_id"] == role_id do
+      %{name: head["user_name"], role: head["description"]}
+    else
+      roles_array(teal, role_id)
+    end
+  end
+
+  def roles_array([], _role_id), do: nil
 
   def get_session(conn) do
     user_id = Plug.Conn.get_session(conn, :user_id)
--- apps/guiltySpark_web/lib/guiltySpark_web/views/layout_view.ex
+++ apps/guiltySpark_web/lib/guiltySpark_web/views/layout_view.ex
@@ -2,8 +2,14 @@ defmodule GuiltySparkWeb.LayoutView do
   use GuiltySparkWeb, :view
   alias GuiltySpark.PermissionHandler
 
-  def user_email(_conn) do
-    "rubeng44@hotmail.com"
+  def user_email(conn) do
+    %{name: name} = PermissionHandler.get_user_names(conn.query_params, conn, "")
+    name
+  end
+
+  def user_role(conn) do
+    %{role: role} = PermissionHandler.get_user_names(conn.query_params, conn, "")
+    role
   end
 
   def valid_session(conn) do
